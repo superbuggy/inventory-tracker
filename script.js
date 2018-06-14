2fe098bbfacc76fa506fdd6b727448380b6778cf
James Reichard <jpqreichard@gmail.com>
2018-06-14 19:34:36 -0400

    Add filtering

--- script.js
+++ script.js
@@ -3,30 +3,36 @@ const DEFAULT_CATEGORY = 'Product Name'
 
 const state = {
   products: [],
-  activeCategory: DEFAULT_CATEGORY
+  activeCategory: DEFAULT_CATEGORY,
+  filterByCountry: null,
+  filteredProducts: []
 }
 
 getProducts().then(data => {
   state.products = data
   drawCategories(data)
   addCountriesToDropDown(data)
+  drawResults(data)
 })
 
 const searchBar = document.querySelector('input.search')
 const searchForm = document.querySelector('form.search')
 const categoriesSection = document.querySelector('div.categories')
+const resultsContainer = document.querySelector('.results')
+const dropDownMenu = document.querySelector('select[name^=country]')
 
-searchBar.addEventListener('input', toggleCategoriesVisibility)
-searchForm.addEventListener('submit', handleSearch)
+dropDownMenu.addEventListener('change', filterByCountry)
+searchBar.addEventListener('input', handleSearch)
+// searchForm.addEventListener('submit', handleSearch)
 
-const FIELDS = [ // These must match spreadsheet headers
+const FIELDS = [ // These must exactly match spreadsheet headers
   'Category',
   'Country',
   'Product Name',
   'Symptom',
   'US Availability',
-  'Active Ingredients(EN)',
-  'Active Ingredients(abroad)'
+  'Active Ingredients (EN)',
+  'Active Ingredients (abroad)'
 ]
 
 function getProducts () {
@@ -37,14 +43,18 @@ function getProducts () {
 function toggleCategoriesVisibility (event) {
   if (event.target.value) {
     categoriesSection.style.opacity = 0
+    resultsContainer.style.opacity = 1
   } else {
     categoriesSection.style.opacity = 1
+    resultsContainer.style.opacity = 0
   }
 }
 
 function handleSearch (event) {
-  event.preventDefault()
-  console.log('Form submitted')
+  toggleCategoriesVisibility(event)
+  const searchTerm = event.target.value.toLowerCase()
+  const productsFilteredBySearchTerm = state.products.filter(product => product[state.activeCategory].toLowerCase().includes(searchTerm))
+  drawResults(productsFilteredBySearchTerm)
 }
 
 function drawCategories (products) {
@@ -73,7 +83,6 @@ function toggleActiveCategory (event) {
     categoryButtons.forEach(categoryButton => categoryButton.classList.remove('active'))
     categoryButton.classList.add('active')
   }
-  console.log(state.activeCategory)
 }
 
 function addCountriesToDropDown (products) {
@@ -82,9 +91,35 @@ function addCountriesToDropDown (products) {
 }
 
 function addCountryToDropDown (country) {
-  const dropDownMenu = document.querySelector('select[name^=country]')
   const newOption = document.createElement('option')
   newOption.value = country
   newOption.innerText = country
   dropDownMenu.appendChild(newOption)
+}
+
+function drawResults (products) {
+  resultsContainer.innerHTML = ''
+  products.forEach(product => drawResult(product))
+}
+
+function drawResult (product) {
+  const div = document.createElement('div')
+  div.classList.add('result')
+  const header = document.createElement('h3')
+  header.innerText = product['Product Name']
+  const ingredientsParagraph = document.createElement('p')
+  ingredientsParagraph.innerText = product['Active Ingredients (EN)']
+  const availabilityParagraph = document.createElement('p')
+  availabilityParagraph.innerText = product['US Availability']
+  const symptomsParagraph = document.createElement('p')
+  symptomsParagraph.innerText = product['Symptom']
+  div.appendChild(header)
+  div.appendChild(ingredientsParagraph)
+  div.appendChild(availabilityParagraph)
+  div.appendChild(symptomsParagraph)
+  resultsContainer.appendChild(div)
+}
+
+function filterByCountry (event) {
+  state.filterByCountry = event.target.value === 'Choose a country...' ? null : event.target.value
 }

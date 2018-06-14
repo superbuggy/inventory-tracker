b578cbff61e0536b7a474a6cbfd29289f134c59a
James Reichard <jpqreichard@gmail.com>
2018-06-14 17:09:35 -0400

    Redo Front End

--- db/seed.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('./connection')
-const Product = require('../models/product')
-
-Product.remove({}).then(_ => {
-  Product.find().then(products => {
-    console.log('products cleared. current products:', products)
-    mongoose.connection.close()
-  })
-})
---
+++ script.js
@@ -0,0 +1,15 @@
+const searchBar = document.querySelector('input.search')
+
+console.log(searchBar)``
+
+const BE_WELL_AWAY_ENDPOINT = 'https://cellmateapp.com/api/gamFYB//BeWellAway'
+
+const FIELDS = [ // These must match spreadsheet headers
+  'Category',
+  'Country',
+  'Product Name',
+  'Symptom',
+  'US Availability',
+  'Active Ingredients(EN)',
+  'Active Ingredients(abroad)'
+]
